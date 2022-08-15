b5982b3401caa558db9d5bfac6ccdb1b3a89795b
Tanner Van Amen <Tvanamen@yahoo.com>
2022-08-15 17:54:29 -0600

    inital commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double Balance = 0;
+
+
+        public void Deposit(double cashIn)
+        {
+          Balance = cashIn + Balance;
+        }
+        public string GetBalance()
+        {
+            return $"${Balance}";
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            Console.WriteLine("Hello please enter amount you will be depositing today!");
+            var cashIn = double.Parse(Console.ReadLine());
+          //while (cashIn != double)
+          //{
+          //    Console.WriteLine("Please enter valid deposit");
+          //}
+            account.Deposit(cashIn);
+            Console.WriteLine($"Thank you for your deposit your balance is now {account.GetBalance()}");
+
+
+
+
+        }
+    }
+}
